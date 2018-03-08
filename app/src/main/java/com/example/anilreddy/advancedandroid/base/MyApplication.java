c12a1e86f04de8ee00227efed723ff9ce151ae69
AnilRdyAleti <AnilRdyAleti@github.com>
2018-03-08 09:24:43 -0500

    Created Advanced android project

---
+++ app/src/main/java/com/example/anilreddy/advancedandroid/base/MyApplication.java
@@ -0,0 +1,15 @@
+package com.example.anilreddy.advancedandroid.base;
+
+import android.app.Application;
+
+/**
+ * Created by anilreddy on 3/8/2018.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
