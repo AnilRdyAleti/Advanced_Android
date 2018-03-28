7a48cd6f957963f67bcdd99a49a126359db05293
AnilRdyAleti <AnilRdyAleti@github.com>
2018-03-28 13:20:22 -0400

    implemented viewmodel and presenter with view

--- app/src/main/java/com/example/anilreddy/advancedandroid/trending/TrendingReposController.java
+++ app/src/main/java/com/example/anilreddy/advancedandroid/trending/TrendingReposController.java
@@ -1,11 +1,18 @@
 package com.example.anilreddy.advancedandroid.trending;
 
-import android.support.annotation.NonNull;
-import android.view.LayoutInflater;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.example.anilreddy.advancedandroid.R;
 import com.example.anilreddy.advancedandroid.base.BaseController;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 
 /**
  * Created by anilreddy on 3/12/2018.
@@ -13,9 +20,50 @@ import com.example.anilreddy.advancedandroid.base.BaseController;
 
 public class TrendingReposController extends BaseController {
 
-    @NonNull
+    @Inject
+    TrendingReposPresenter presenter;
+    @Inject
+    TrendingReposViewModel viewModel;
+
+    @BindView(R.id.repo_list)
+    RecyclerView repoList;
+    @BindView(R.id.loading_indicator)
+    ProgressBar loadingView;
+    @BindView(R.id.tv_error)
+    TextView errorText;
+
     @Override
-    protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
-        return null;
+    protected int layoutRes() {
+        return R.layout.screen_trending_repos;
+    }
+
+    @Override
+    protected Disposable[] subscriptions() {
+        return new Disposable[]{
+                viewModel.loading()
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(loading -> {
+                    repoList.setVisibility(loading ? View.VISIBLE : View.GONE);
+                    loadingView.setVisibility(loading ? View.VISIBLE : View.GONE);
+                    errorText.setVisibility(loading ? View.VISIBLE : View.GONE);
+                }),
+                viewModel.repos()
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(loading -> {
+                    //TODO
+                }),
+                viewModel.error()
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(errorRes -> {
+                    if (errorRes == -1) {
+                        errorText.setText(null);
+                        errorText.setVisibility(View.GONE);
+                    } else {
+                        errorText.setText(errorRes);
+                        errorText.setVisibility(View.VISIBLE);
+                        repoList.setVisibility(View.GONE);
+                    }
+                })
+        };
     }
 }
